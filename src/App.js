b6cf5155a78c70b22ed5ca5f89b1512309640261
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2024-05-02 18:02:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
 
 function App() {
+  const [color, setColor] = useState("#000000");
+  const [opacity, setOpacity] = useState(1);
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const handleOpacityChange = (event) => {
+    const opacityValue = Number(event.target.value);
+    setOpacity(opacityValue);
+  };
+
+  const getRGB = () => {
+    const red = parseInt(color.slice(1, 3), 16);
+    const green = parseInt(color.slice(3, 5), 16);
+    const blue = parseInt(color.slice(5, 7), 16);
+
+    return `rgb(${red},${green},${blue})`;
+  };
+
+  const getOpacityPercentage = () => {
+    const opacityPercentage = Math.round(opacity * 100);
+    return `${opacityPercentage}%`;
+  };
+
+  const getCSSCode = () => {
+    const cssCode = `background-color:${color};
+    opacity:${opacity}`;
+
+    return cssCode.trim();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Color Generator</h1>
+      <input type="color" value={color} onChange={handleColorChange} />
+      <br />
+      <input
+        type="range"
+        min="0"
+        max="1"
+        step="0.1"
+        value={opacity}
+        onChange={handleOpacityChange}
+      />
+      {color && (
+        <div
+          className="color-box"
+          style={{ backgroundColor: color, opacity: opacity }}
+        ></div>
+      )}
+      {color && (
+        <div className="color-info">
+          <p>Hex:{color}</p>
+          <p>RGB:{getRGB()}</p>
+          <p>Opacity:{getOpacityPercentage()}</p>
+          <pre>
+            <code>{getCSSCode()}</code>
+          </pre>
+        </div>
+      )}
     </div>
   );
 }
